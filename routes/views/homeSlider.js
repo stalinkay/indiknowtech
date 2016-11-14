5a36febf88809eb17fd94334b622debfdd3f3cb5
bertie <coetzeealbertus@gmail.com>
2016-11-14 16:59:03 +0200

    Bug Fix

--- routes/views/homeSlider.js
+++ routes/views/homeSlider.js
@@ -16,44 +16,6 @@ exports = module.exports = function (req, res) {
 		categories: [],
 	};
 
-	// Load all categories
-	view.on('init', function (next) {
-
-		keystone.list('HomeSliderCategory').model.find().sort('name').exec(function (err, results) {
-
-			if (err || !results.length) {
-				return next(err);
-			}
-
-			locals.data.categories = results;
-
-			// Load the counts for each category
-			async.each(locals.data.categories, function (category, next) {
-
-				keystone.list('HomeSlider').model.count().where('categories').in([category.id]).exec(function (err, count) {
-					category.postCount = count;
-					next(err);
-				});
-
-			}, function (err) {
-				next(err);
-			});
-		});
-	});
-
-	// Load the current category filter
-	view.on('init', function (next) {
-
-		if (req.params.category) {
-			keystone.list('HomeSliderCategory').model.findOne({ key: locals.filters.category }).exec(function (err, result) {
-				locals.data.category = result;
-				next(err);
-			});
-		} else {
-			next();
-		}
-	});
-
 	// Load the posts
 	view.on('init', function (next) {
 
